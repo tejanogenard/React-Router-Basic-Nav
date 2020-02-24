da4309841d614cae8dc6f071277b3d4eb516ccc8
Genard Ivan Tejano <genardivanteajno@Genards-MacBook-Pro.local>
2020-02-24 13:21:21 -0800

    Route path set up

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
+import { Route } from "react-router-dom";
 
 const App = () => (
   <div>
-    <Navigation />
+    
+    <Navigation >
+       <Route path = "/"><Home /></Route>
+       <Route path = "/About"> <About/></Route>
+       <Route path = "/Contact"><Contact /></Route>
+     </Navigation>
+ 
   </div>
 );
 
